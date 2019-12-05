b8c03da28795b64a08c62c7de63425d86b6889df
SiebenAcht <jfr@null.net>
2019-12-05 14:56:06 +0000

    test

--- init.el
+++ init.el
@@ -10,4 +10,5 @@
 ;; chad settings, those who have different ones are inferior beings
 (evil-mode 1) ;keybinds
 (setq-default indent-tabs-mode t) ;tab indents
+(setq make-backup-files nil) ;; Backup files suck. "I don't recommend this last one" - stackexchange guy who taught me this.
 (setq tab-width 8) ;REAL tab indents
